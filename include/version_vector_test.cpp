bd4d7744d17138181deaf13cabf0d64a8a846a4e
Maxim Eryomenko <moeryomenko@gmail.com>
2021-07-15 10:57:39 +0300

    fix: insert instead raw loop
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- include/version_vector_test.cpp
+++ include/version_vector_test.cpp
@@ -4,20 +4,11 @@
 #include <rapidcheck.h>
 #include <unordered_map>
 
-template<typename K, typename V>
-auto stdMapToRobinHood(std::unordered_map<K, V> stdmap) -> robin_hood::unordered_flat_map<K, V> {
-    robin_hood::unordered_flat_map<K, V> res;
-    for (auto [k, v]: stdmap) {
-        res[k] = v;
-    }
-    return res;
-}
-
 auto main() -> int
 {
     rc::check("increment method return the dot incremented by one",
             [](std::unordered_map<int, uint32_t> dots) {
-                version_vector<int> v{std::move(stdMapToRobinHood(dots))};
+                version_vector<int> v(robin_hood::unordered_flat_map<int, uint32_t>(dots.begin(), dots.end()));
                 for(auto [actor, counter] : v.dots) {
                     auto incremanted_dot = dot{actor, counter+1};
                     RC_ASSERT(v.increment(actor) == incremanted_dot);
