0d0a6a161e6e23146c970178ae79fe35923d3bbc
Maxim Eryomenko <moeryomenko@gmail.com>
2021-07-15 15:46:17 +0300

    test: assert result of test
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- include/version_vector_test.cpp
+++ include/version_vector_test.cpp
@@ -6,13 +6,13 @@
 
 auto main() -> int
 {
-    rc::check("increment method return the dot incremented by one",
+    assert(rc::check("increment method return the dot incremented by one",
             [](std::unordered_map<int, uint32_t> dots) {
                 version_vector<int> v(robin_hood::unordered_flat_map<int, uint32_t>(dots.begin(), dots.end()));
                 for(auto [actor, counter] : v.dots) {
                     auto incremanted_dot = dot{actor, counter+1};
                     RC_ASSERT(v.increment(actor) == incremanted_dot);
                 }
-            });
+            }));
     return 0;
 }
