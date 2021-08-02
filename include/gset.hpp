63eef8d28605569c9b59f89d9144c11e97d2532e
Maxim Eryomenko <moeryomenko@gmail.com>
2021-08-02 20:19:03 +0300

    feat: add grow-only set
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ include/gset.hpp
@@ -0,0 +1,43 @@
+#ifndef GSET_H
+#define GSET_H
+
+#include <robin_hood.h>
+
+#include <crdt_traits.hpp>
+
+template<value_type T>
+struct gset {
+    robin_hood::unordered_flat_set<T> value;
+
+    gset() = default;
+    gset(const gset<T>&) = default;
+    gset(gset<T>&&) = default;
+
+    auto validate_merge(const gset<T>&) noexcept
+        -> std::optional<std::error_condition> { return std::nullopt; }
+
+    void merge(const gset<T>& other) noexcept {
+        value.merge(other.value);
+    }
+
+    auto validate_op(const T& val) noexcept
+        -> std::optional<std::error_condition> { return std::nullopt; }
+
+    void apply(const T& val) noexcept {
+        insert(val);
+    }
+
+    void insert(const T& val) noexcept {
+        value.insert(val);
+    }
+
+    bool contains(const T& val) noexcept {
+        return value.contains(val);
+    }
+
+    auto read() -> robin_hood::unordered_flat_set<T> {
+        return value;
+    }
+};
+
+#endif // GSET_H
